07669d99f13925017976d277a3b83cdd1759953e
Rajkishor Kumar <rajkishorkumar@Rajkishors-MacBook-Air.local>
2025-10-05 18:28:05 +0530

    added memorizarion approach

--- 1-D DP/src/ClimbStairs.java
+++ 1-D DP/src/ClimbStairs.java
@@ -4,9 +4,17 @@ public class ClimbStairs {
 
     public static void main(String[] args) {
         int n=2;
+        int dp[]=new int[n+1];
         System.out.println(climbStairsRecursive(n));
+        System.out.println((climbStairsMemo(n,dp)));
 
+    }
 
+    private static int climbStairsMemo(int n,int[] dp) {
+        if(n==1||n==0) return 1;
+        if(dp[n]!=0) return dp[n];
+        dp[n]=climbStairsMemo(n-1,dp)+climbStairsMemo(n-2,dp);
+        return dp[n];
     }
 
     private static int climbStairsRecursive(int n) {
